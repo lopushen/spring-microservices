e48221d14bd14e7a4fa6c0e876e91849f87141d7
i.lopushen <i.lopushen@levi9.com>
2016-07-11 10:56:44 +0300

    initial commit

---
+++ src/test/java/com/lopushen/SpringMicroservicesApplicationTests.java
@@ -0,0 +1,18 @@
+package com.lopushen;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SpringMicroservicesApplication.class)
+@WebAppConfiguration
+public class SpringMicroservicesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
